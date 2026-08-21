629f763fb897b361c62bf8c9a2b28e62e1953871
Harsh Agarwal <harshagarwal31.creator@gmail.com>
2026-08-21 10:54:18 +0530

    just learned the prompt section of mcp

--- first_mcp_server/src/first_mcp/server.py
+++ first_mcp_server/src/first_mcp/server.py
@@ -36,5 +36,29 @@ def server_status() -> str:
     return "server is running normally"
 
 
+# dynamic resources
+
+
+@mcp.resource("user://status/{username}")
+def user_info(username: str) -> str:
+    """get information about user"""
+    return f"user : {username}"
+
+
+# mcp prompts
+
+
+@mcp.prompt
+def debug_code(code: str) -> str:
+    """create a prompt to debug the code"""
+    return f""" you are the python developer
+                analyze the following python code 
+                {code}
+ 
+                resolve the logical , syntax and other mistakes
+
+            """
+
+
 if __name__ == "__main__":
     mcp.run()
