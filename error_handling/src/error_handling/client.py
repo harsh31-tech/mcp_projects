0438f80f23e3ec2ca08efe777a16eaecdb3cb44f
Harsh Agarwal <harshagarwal31.creator@gmail.com>
2026-08-31 14:29:13 +0530

    learned the use of pydantic in mcp for validation of inputs

--- error_handling/src/error_handling/client.py
+++ error_handling/src/error_handling/client.py
@@ -4,15 +4,22 @@ from fastmcp import Client
 
 
 async def main():
-    client = Client("src/error_handling/server.py")
+    client = Client("server.py")
+
+    # async with client:
+    #     try:
+    #         divide = await client.call_tool("divide", {"a": 10, "b": 0})
+    #         print(divide)
+    #     except Exception as e:
+    #         print("tool failed :", e)
 
     async with client:
-        try:
-            divide = await client.call_tool("divide", {"a": 10, "b": 0})
-            print(divide)
-        except Exception as e:
-            print("tool failed :" , e)
-        
+        username = await client.call_tool(
+            "create_user",
+            {"user": {"name": "harsh", "email": "harsh@gmail.com", "age": 18}},
+        )
+        print(username)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- error_handling/src/error_handling/server.py
+++ error_handling/src/error_handling/server.py
@@ -1,16 +1,29 @@
 from fastmcp import FastMCP, Context
+from pydantic import BaseModel
 import asyncio
 
 mcp = FastMCP(name="error handling server")
 
 
+class User(BaseModel):
+    name: str
+    age: int
+    email: str
+
+
+# @mcp.tool
+# def divide(a: float, b: float) -> float:
+#     """divide two numbers"""
+#     if b == 0:
+#         raise ValueError("cannot divide by zero")
+
+#     return a / b
+
+
 @mcp.tool
-def divide(a: float, b: float) -> float:
-    """divide two numbers"""
-    if b==0:
-        raise ValueError("cannot divide by zero")
-    
-    return a / b
+def create_user(user: User) -> str:
+    """create a user"""
+    return f"user {user.email} of age {user.age} is created successfully"
 
 
 if __name__ == "__main__":
