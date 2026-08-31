04db316361a66d12552f2a45229c09c69e49d935
Harsh Agarwal <harshagarwal31.creator@gmail.com>
2026-08-31 11:39:09 +0530

    just learned the progress context of mcp

--- first_mcp_server/src/first_mcp/client.py
+++ first_mcp_server/src/first_mcp/client.py
@@ -2,16 +2,22 @@ import asyncio
 
 from fastmcp import Client
 
+
 async def main():
     client = Client("src/first_mcp/server.py")
-    
+
     async with client:
         result = await client.call_tool(
-            "generate_code",
-            {"project_name": "express js backend"}
+            "generate_code", {"project_name": "express js backend"}
         )
         print(result)
-    
+
+    async with client:
+        progress = await client.call_tool(
+            "generation_progress", {"project_name": "express js backend"}
+        )
+        print(progress)
+
+
 if __name__ == "__main__":
     asyncio.run(main())
-    
--- first_mcp_server/src/first_mcp/server.py
+++ first_mcp_server/src/first_mcp/server.py
@@ -1,5 +1,6 @@
-from fastmcp import FastMCP , Context
+from fastmcp import FastMCP, Context
 from fastmcp.prompts import Message
+import asyncio
 
 
 mcp = FastMCP(name="first_server")
@@ -60,46 +61,47 @@ def debug_code(code: str) -> str:
                 resolve the logical , syntax and other mistakes
 
             """
-#myltiple inputs in prompt
+
+
+# myltiple inputs in prompt
 @mcp.prompt
-def code_review(code:str,
-                language :str,
-                focus:str)->str:
-    """ create a prompt to review the code """
+def code_review(code: str, language: str, focus: str) -> str:
+    """create a prompt to review the code"""
     return f""" you are a senior developer 
                 review the following code
                 {code}
                 it is based on the lanaguage {language} 
                 and output is foused on {focus}"""
-                
-# using python message object to parse the message 
+
+
+# using python message object to parse the message
 @mcp.prompt
-def structured_prompt(code:str)->list[Message]:
-    """ create a structured prompt """
+def structured_prompt(code: str) -> list[Message]:
+    """create a structured prompt"""
     return [
         Message(
             role="system",
-            content = "you are a expert python devloper ", 
+            content="you are a expert python devloper ",
         ),
-        Message(
-            role = "user",
-            content = f"analyze the code of python {code}"
-        )
+        Message(role="user", content=f"analyze the code of python {code}"),
     ]
+
 
 # learning context
 
-@mcp.tool
-async def process_data(data:str, ctx:Context)->str:
-    """ process data and demonstrate mcp context """
-    await ctx.info(f"processing {data}")
-    
-    return f"proccessed {data}"
 
 @mcp.tool
-async def generate_code(project_name : str , ctx : Context)->str:
+async def process_data(data: str, ctx: Context) -> str:
+    """process data and demonstrate mcp context"""
+    await ctx.info(f"processing {data}")
+
+    return f"proccessed {data}"
+
+
+@mcp.tool
+async def generate_code(project_name: str, ctx: Context) -> str:
     """generate the backend project"""
-    
+
     await ctx.info("validation project structure")
 
     await ctx.info("Creating project structure")
@@ -109,8 +111,37 @@ async def generate_code(project_name : str , ctx : Context)->str:
     await ctx.info("Generating database layer")
 
     await ctx.info("Generating authentication")
-    
+
     return f"the backend project is created {project_name}"
+
+
+@mcp.tool
+async def generation_progress(project_name: str, ctx: Context) -> str:
+    """generate project step by step"""
+    await ctx.info("Creating project structure")
+    await ctx.report_progress(0, 5)
+
+    await asyncio.sleep(2)
+
+    await ctx.info("Generating API routes")
+    await ctx.report_progress(1, 5)
+
+    await asyncio.sleep(2)
+
+    await ctx.info("Generating database layer")
+    await ctx.report_progress(3, 5)
+
+    await asyncio.sleep(2)
+
+    await ctx.info("Generating authentication")
+    await ctx.report_progress(4, 5)
+
+    await asyncio.sleep(2)
+
+    await ctx.info("Project generation complete")
+    await ctx.report_progress(5, 5)
+
+    return f"the backend project is generated{project_name}"
 
 
 if __name__ == "__main__":
